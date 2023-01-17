18424f7b512238082336bab1fc905dade6a66bc5
Khanhlam1186 <khanhyt2002@gmail.com>
2023-01-18 00:43:45 +0700

    upload src code

---
+++ index.js
@@ -0,0 +1,14 @@
+let cnt=1;
+function cap1(){
+    if(cnt==3){
+        alert("Anh yeu em")
+        cnt=1;
+    }else if(cnt==2){
+        alert("Chúc vợ yêu năm mới luôn vui vẻ, xinh đẹp , thành công trong mọi việc và luôn hạnh phúc bên chồng và những người thân. ")
+        cnt++;
+    }
+    else{
+        alert("Một năm trôi qua nhanh như cách anh nhớ em vậy. ")
+        cnt++;
+    } 
+}
